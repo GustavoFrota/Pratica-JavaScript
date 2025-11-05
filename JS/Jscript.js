4e9b1e92b31121e1ead506c55d12c5053504c647
GustavoFrota <gustavofro90@gmail.com>
2025-11-05 15:01:47 -0300

    Atualização 1.2

--- JS/Jscript.js
+++ JS/Jscript.js
@@ -5,3 +5,9 @@ let Anterior = prompt("Digite o ano em que você nasceu")
 Anterior = Number(Anterior)
 let Idade = Atual - Anterior
 alert(`Você tem ${Idade} anos!`)
+if(Idade >= 18) {
+    alert("Você é adulto.")
+}
+else {
+    alert("Você é uma criança.")
+}
