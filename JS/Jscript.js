db7ce228a43779eccac6fe76fead45a99f6a2333
GustavoFrota <gustavofro90@gmail.com>
2025-11-07 09:03:15 -0300

    Pequena alteração

--- JS/Jscript.js
+++ JS/Jscript.js
@@ -1,7 +1,7 @@
 alert("Olá")
-let Atual = prompt("Digite o ano atual")
+let Atual = prompt("Digite o ano atual: ")
 Atual = Number(Atual)
-let Anterior = prompt("Digite o ano em que você nasceu")
+let Anterior = prompt("Digite o ano em que você nasceu: ")
 Anterior = Number(Anterior)
 let Idade = Atual - Anterior
 alert(`Você tem ${Idade} anos!`)
