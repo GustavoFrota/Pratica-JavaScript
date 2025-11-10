c6e4f6556574efdbcf4a3b946e3c688abca7a475
GustavoFrota <gustavofro90@gmail.com>
2025-11-10 10:20:47 -0300

    Correção

--- JS/Jscript.js
+++ JS/Jscript.js
@@ -3,11 +3,12 @@ let dia_aniversario = prompt("Digite o dia do seu aniversário: ")
 dia_aniversario = Number(dia_aniversario)
 let mes_aniversario = prompt("Digite o mês do seu aniversário: ")
 mes_aniversario = Number(mes_aniversario)
+let festa
 
 if(isNaN(dia_aniversario) || isNaN(mes_aniversario)) {
   alert("Por favor digite apenas números")
 } else {
-let festa = [dia_aniversario, mes_aniversario] }
+ festa = [dia_aniversario, mes_aniversario] }
 
 let dia = prompt("Digite o dia: ")
 dia = Number(dia)
@@ -23,4 +24,4 @@ if(festa[0] === surpresa[0] && festa[1] === surpresa[1]) {
   alert("Feliz aniversário!")
 } else {
   alert("Hoje não é seu aniversário")
-} }//Ainda não está funcionando 100% 
+} }
