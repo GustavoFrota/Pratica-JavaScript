c4aba15307a33c7a6b0ca449f836253b6b595b8a
GustavoFrota <gustavofro90@gmail.com>
2025-11-19 14:43:30 -0300

    Atualização no JavaScript

--- JS/Jscript.js
+++ JS/Jscript.js
@@ -3,57 +3,34 @@ let instituição = document.getElementById("escola").value
 let Nome = document.getElementById("nome").value
 let Sobrenome = document.getElementById("sobrenome").value
 let serie = document.getElementById("serie").value
-alert(`Bem-vindo(a) ${Nome} ${Sobrenome} ao Sistema de Notas da Escola ${instituição}`)
-}
 
-let nota1 = prompt("Digite a nota de português: ")
-nota1 = Number(nota1)
-let nota2 = prompt("Digite a nota de inglês: ")
-nota2 = Number(nota2)
-let nota3 = prompt("Digite a nota de matemática: ")
-nota3 = Number(nota3)
-let nota4 = prompt("Digite a nota de filosofia: ")
-nota4 = Number(nota4)
-let nota5 = prompt("Digite a nota de redação: ")
-nota5 = Number(nota5)
-let nota6 = prompt("Digite a nota de sociologia: ")
-nota6 = Number(nota6)
-let nota7 = prompt("Digite a nota de geografia: ")
-nota7 = Number(nota7)
-let nota8 = prompt("Digite a nota de química: ")
-nota8 = Number(nota8)
-let nota9 = prompt("Digite a nota de física: ")
-nota9 = Number(nota9)
-let nota10 = prompt("Digite a nota de biologia: ")
-nota10 = Number(nota10)
-let nota11 = prompt("digite a nota de artes: ")
-nota11 = Number(nota11)
-let nota12 = prompt("Digite a nota de educaão física: ")
-nota12 = Number(nota12)
+
+
+let nota1 = document.getElementById("nota1").value
+let nota2 = document.getElementById("nota2").value
+let nota3 = document.getElementById("nota3").value
+let nota4 = document.getElementById("nota4").value
+let nota5 = document.getElementById("nota5").value
+let nota6 = document.getElementById("nota6").value
+let nota7 = document.getElementById("nota7").value
+let nota8 = document.getElementById("nota8").value
+let nota9 = document.getElementById("nota9").value
+let nota10 = document.getElementById("nota10").value
+let nota11 = document.getElementById("nota11").value
+let nota12 = document.getElementById("nota12").value
 
 if(isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4) || isNaN(nota5) || isNaN(nota6) || isNaN(nota7) || isNaN(nota8) || isNaN(nota9) || isNaN(nota10) || isNaN(nota11) || isNaN(nota12)) {
-  alert("Por favor, digite apenas números válidos")  
-} else {
-let media = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6 + nota7 + nota8 + nota9 + nota10 + nota11 + nota12) / 12
-let media_formatada = media.toFixed(1)
-alert(`A média de ${Nome} da série ${serie} foi ${media_formatada}`) 
-
-switch(true) {
-    case(media > 10):
-     alert("Média acima de 10. Tente novamente")
-    break;
-    case(media == 10):
-     alert(`Exelente! Continue assim ${Nome}.`)
-     break;
-    case(media >= 7):
-     alert("Aprovado")
-     break;
-    case(media >= 5):
-     alert("Recuperação")
-     break;
-    default:
-        alert("Reprovado")
-        break;
-
+document.getElementById("resultado").innerHTML = " Preencha todas as notas."
 }
+
+const media = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6 + nota7 + nota8 + nota9 + nota10 + nota11 + nota12) / 12
+let situacao = " "
+if (media >= 7) {
+  situacao = " Aprovado"
+} else if (media >=5) {
+  situacao = " Recuperação"
+} else {
+  situacao = " reprovado"
+}
+document.getElementById("resultado").innerHTML = `A situação de ${Nome} ${Sobrenome} da série ${serie} é ${situacao}`
 }
